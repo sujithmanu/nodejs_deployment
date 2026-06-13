b443f725f1c1cbb0b68e1de1a5fe359a3ceaf918
sujithmanu <147632913+sujithmanu@users.noreply.github.com>
2026-06-13 12:25:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Welcome to sterna');
+  res.send('Welcome to sterna security');
 });
 
 app.listen(3000, () => {
